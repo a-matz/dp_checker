fa72c370a166528ffd4f6db528e133e883bbab77
a-matz <96798952+a-matz@users.noreply.github.com>
2023-02-18 00:32:21 +0100

    load files to table

--- dp_checker_dialog.py
+++ dp_checker_dialog.py
@@ -25,8 +25,10 @@
 import os
 import glob
 from datetime import datetime
+import pandas as pd
+import numpy as np
 from qgis.PyQt import uic
-from qgis.PyQt import QtWidgets
+from qgis.PyQt import QtWidgets, QtGui
 from qgis.core import QgsMapLayerProxyModel
 from qgis.PyQt.QtCore import Qt, QSignalBlocker
 from qgis.PyQt.QtWidgets import QTableWidgetItem
@@ -51,13 +53,30 @@ class dpCheckerDialog(QtWidgets.QDialog, FORM_CLASS):
         self.fileWidget.fileChanged.connect(self.check_dp_path)
 
         self.combobox_maplayer.setFilters(QgsMapLayerProxyModel.LineLayer)
-        self.combobox_maplayer.layerChanged.connect(self.combobox_haltungnr.setLayer)
 
-        with QSignalBlocker(self.combobox_maplayer):
-            self.combobox_maplayer.setCurrentIndex(-1)
+        self.combobox_attribute = [
+            self.combobox_haltungnr,
+            self.combobox_vonSchacht,
+            self.combobox_bisSchacht,
+            self.combobox_dn,
+            self.combobox_laenge,
+            self.combobox_material
+        ]
 
+        for combobox in self.combobox_attribute:
+            self.combobox_maplayer.layerChanged.connect(combobox.setLayer)
+            self.combobox_maplayer.layerChanged.connect(lambda layer, combobox = combobox: self.combobox_reset_index(combobox))
+        
+        self.combobox_maplayer.setCurrentIndex(-1)
+        
+
+    def combobox_reset_index(self,combobox):
+        combobox.setCurrentIndex(-1)
 
     def check_dp_path(self):
+        """
+        if path is valid enable button to load files
+        """
         if os.path.isdir(self.fileWidget.filePath()):
             self.button_load_dp.setEnabled(True)
         else:
@@ -69,35 +88,92 @@ class dpCheckerDialog(QtWidgets.QDialog, FORM_CLASS):
          return time
     
     def load_dp_files(self):
+        """
+        load all .sew files
+        try to read all attributes - if attribute is not found None is assignes
+        combine all files to pandas dataframe and pass it to update_table function
+        """
         files = glob.glob(f"{self.fileWidget.filePath()}/*.sew")
-        dp_dict = {}
+        
+        # list to store dictionaries 
+        df_list = []
         for dp_file in files:
+            dp_dict = {}
             xtree = et.parse(dp_file)
-
             #protocol data
             protocol = xtree.find("document").find("data").find("protocol")
-            dp_dict["reach"] = protocol.find("examReach").get("value")
-            dp_dict["upperManhole"] = protocol.find("upperManhole").get("value")
-            dp_dict["lowerManhole"] = protocol.find("lowerManhole").get("value")
-            dp_dict["dn"] =  protocol.find("sewerDn").get("value")
-            dp_dict["length"]  = protocol.find("sewerLength").get("value")
-            dp_dict["material"] = protocol.find("sewerMaterialAtv").get("value")
-            dp_dict["examPassed"] = protocol.find("examPassed").get("value")
-            dp_dict["classification"] = protocol.find("classification").get("value")
+            try:
+                dp_dict["Bezeichnung_original"] = protocol.find("examReach").get("value")
+            except:
+                dp_dict["Bezeichnung_original"] = None
+            try:
+                dp_dict["Bezeichnung"] = protocol.find("examReach").get("value").split("/")[0].strip()
+            except:
+                dp_dict["Bezeichnung"] = None
+            try:
+                dp_dict["Schacht oben"] = protocol.find("upperManhole").get("value")
+            except:
+                dp_dict["Schacht oben"] = None
+            try:
+                dp_dict["Schacht unten"] = protocol.find("lowerManhole").get("value")
+            except:
+                dp_dict["Schacht unten"] = None
+            try:
+                dp_dict["DN"] = int(protocol.find("sewerDn").get("value"))
+            except:
+                dp_dict["DN"] = None
+            try:
+                dp_dict["Länge"]  = round(float(protocol.find("sewerLength").get("value")),2)
+            except:
+                dp_dict["Länge"] = None
+            try:
+                dp_dict["Material kürzel"] = protocol.find("sewerMaterialAtv").get("value")
+            except:
+                dp_dict["Material kürzel"] = None
+            try:
+                dp_dict["Material"] = protocol.find("sewerMaterial").get("value")
+            except:
+                dp_dict["Material"] = None
+            try:
+                dp_dict["Prüfung bestanden"] = bool(protocol.find("examPassed").get("value"))
+            except:
+                dp_dict["Prüfung bestanden"] = None
+            try:
+                dp_dict["Ergebnis"] = protocol.find("classification").get("value")
+            except:
+                dp_dict["Ergebnis"] = None
             #measurement data
             measurement= xtree.find("document").find("data").find("measurement")
-            dp_dict["pressureChange"] = measurement.find("pressureChange").get("value")
-            dp_dict["calmDuration"] = datetime.strptime(measurement.find("calmDuration").get("value"), "%H:%M:%S")
-            dp_dict["examDuration"] = datetime.strptime(measurement.find("examDuration").get("value"),"%H:%M:%S")
-            #gps position at stop
-            gps_position_string = measurement.find("stop").find("gps").get("value")
-            gps_list = gps_position_string.split(" ")
-            dp_dict["gps_n"] = gps_list[6]
-            dp_dict["gps_e"] = gps_list[10]
+            try:
+                dp_dict["Druckänderung"] = round(float(measurement.find("pressureChange").get("value")),2)
+            except:
+                dp_dict["Druckänderung"] = None
+            try:
+                dp_dict["Prüfdauer"] = datetime.strptime(measurement.find("examDuration").get("value"),"%H:%M:%S")
+            except:
+                dp_dict["Prüfdauer"] = None
+            try:
+                dp_dict["Beruhigungszeit"] = datetime.strptime(measurement.find("calmDuration").get("value"), "%H:%M:%S")
+            except:
+                dp_dict["Beruhigungszeit"] = None
 
-            for key, value in dp_dict.items():
-                print(f"{key}: {value}")
-            self.update_table(dp_dict)
+            #gps position at stop
+            try:
+                gps_position_string = measurement.find("stop").find("gps").get("value")
+                gps_list = gps_position_string.split(" ")
+                dp_dict["GPS N"] = float(gps_list[6])
+                dp_dict["GPS E"] = float(gps_list[10])
+            except:
+                dp_dict["GPS N"] = None
+                dp_dict["GPS E"] = None
+            dp_dict["Datei"] = os.path.basename(dp_file)
+
+
+            df_list.append(dp_dict)
+        # create dataframe from list with dictionaries
+        self.dp_table = pd.DataFrame(df_list)
+        # update tableviewwidget
+        self.update_table(self.dp_table)
     
     def update_table(self, data):
         with QSignalBlocker(self.table) as block:
@@ -106,17 +182,21 @@ class dpCheckerDialog(QtWidgets.QDialog, FORM_CLASS):
             self.table.clear()
             self.table.setSortingEnabled(False)
             self.table.setRowCount(0)
-            self.table.setColumnCount(len(data))
-            self.table.setRowCount(1)        
+            self.table.setColumnCount(len(data.columns))
+            self.table.setRowCount(len(data))        
             
-            for i, (key, value) in enumerate(data.items()):
-                item = QTableWidgetItem()
-                if "Duration" in key:
-                    value = value.strftime("%H:%M:%S")
-                item.setData(Qt.EditRole, value)
-                self.table.setItem(0,i,item)
+            for i, row in data.iterrows():
+                for col,value in enumerate(row):
+                    if value != None and not pd.isnull(value):
+                        if isinstance(value,pd.datetime):
+                            value = value.strftime("%H:%M:%S")
+                        item = QTableWidgetItem()
+                        
+                        #item.setData(Qt.EditRole, value)
+                        item.setData(Qt.DisplayRole, value)
+                        self.table.setItem(i,col,item)
             
-            self.table.setHorizontalHeaderLabels(data.keys())
+            self.table.setHorizontalHeaderLabels(data.columns.values)
             #self.table.setColumnHidden(self.attributauswahl.index("fid"), True)
             self.table.resizeColumnsToContents()
             self.table.setSortingEnabled(True)
